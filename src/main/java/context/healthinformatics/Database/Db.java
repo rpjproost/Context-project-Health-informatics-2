f02dcc2ff44d05f3c75ab8c7699459fb2ae7717a
rpjproost <rickproost181@hotmail.com>
2015-04-30 15:17:43 +0200

    - added basic database functionality - select method needs work!

--- src/main/java/context/healthinformatics/Database/Db.java
+++ src/main/java/context/healthinformatics/Database/Db.java
@@ -13,15 +13,17 @@ import java.sql.Statement;
  */
 public class Db {
 
-	private String db;
+	private String db; //db query.
+	private String pad = "C:/db/analyze"; //default path for testing.
+	private Connection conn;
+	private Statement stmt = null;
 
 	/**
 	 * Sets up connection.
 	 * @throws SQLException 
 	 */
 	public void setupConn() {
-		db = ("jdbc:derby:C:/db/analyzea;create=true");
-		Connection conn;
+		setDb(pad);
 		try {
 			conn = DriverManager.getConnection(db);
 			//Check connection
@@ -38,10 +40,133 @@ public class Db {
 
 	/**
 	 * 
+	 * @param tableName name for new table.
+	 * @param columns column names.
+	 * @param types type specifications.
+	 */
+	public void createTable(String tableName, String[] columns, String[] types) {
+		if (conn != null) {
+			try {
+				stmt = conn.createStatement();
+				String sql = "CREATE TABLE " + tableName + createTableColumns(columns, types);
+				stmt.executeUpdate(sql);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("Connection is not set.");
+		}
+	}
+
+	/**
+	 * 
+	 * @param columns column names.
+	 * @param types type specifications.
+	 * @return string for sql building.
+	 */
+	public String createTableColumns(String[] columns, String[] types) {
+		String res = "(";
+		for (int i = 0; i < columns.length; i++) {
+			if (i == columns.length - 1) {
+				res = res + columns[i] + " ";
+				res = res + types[i] + ")";
+			}
+			else {
+				res = res + columns[i] + " ";
+				res = res + types[i] + ",";
+			}
+		}
+		return res;
+	}
+
+	/**
+	 * 
+	 * @param tableName insert into this table.
+	 * @param values variables to insert.
+	 */
+	public void insert(String tableName, String[] values) {
+		if (conn != null) {
+			try {
+				stmt = conn.createStatement();
+				String sql = "INSERT INTO " + tableName + " VALUES (";
+				for (int i = 0; i < values.length; i++) {
+					if (i == values.length - 1) {
+						sql = sql + values[i] + ")";
+					}
+					else {
+						sql = sql + values[i] + ",";
+					}
+				}
+				stmt.executeUpdate(sql);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("Connection is not set.");
+		}
+	}
+	
+	/**NEEDS WORK!
+	 * 
+	 * @param tableName name of table to select variables from.
+	 * @param variable variables to select.
+	 * @return String with results.
+	 */
+	public String select(String tableName, String variable) {
+		String res = "";
+		if (conn != null) {
+			try {
+				stmt = conn.createStatement();
+				String sql = "SELECT " + variable + " FROM " + tableName;
+				ResultSet rs = stmt.executeQuery(sql);
+				while (rs.next()) {
+					res = rs.getString("test2");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("Connection is not set.");
+		}
+		return res;
+	}
+
+	/**
+	 * 
+	 * @param tableName name of table to drop.
+	 */
+	public void dropTable(String tableName) {
+		if (conn != null) {
+			try {
+				stmt = conn.createStatement();
+				String sql = "DROP TABLE " + tableName;
+				stmt.executeUpdate(sql);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("Connection is not set.");
+		}
+	}
+
+	/**
+	 * 
 	 * @return db path string.
 	 */
 	public String getDbPath() {
 		return db;
+	}
+
+	/**
+	 * 
+	 * @param path sets db path
+	 */
+	public void setDbPath(String path) {
+		pad = path; 
 	}
 
 	/**
--- src/main/java/context/healthinformatics/Main.java
+++ src/main/java/context/healthinformatics/Main.java
@@ -1,16 +1,15 @@
 package context.healthinformatics;
 
-import java.sql.SQLException;
+import java.io.IOException;
 
-import context.healthinformatics.Database.Db;
 import context.healthinformatics.Parser.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		//XMLParser xmlp = new XMLParser("src/main/data/ExampleStatSensor.xml");
-		//xmlp.parse();
-		Db database = new Db();
-		database.setupConn();
+	public static void main(String[] args) throws IOException {
+		XMLParser xmlp = new XMLParser("src/main/data/ExampleStatSensor.xml");
+		xmlp.parse();
+		TXTParser txtp = new TXTParser(xmlp.getPath(),xmlp.getStartLine(),xmlp.getDelimiter(), xmlp.getColumns());
+		txtp.parse();
 	}
 }
