1211c56f39085c4ec070513478c35d0eeb47918c
rpjproost <rickproost181@hotmail.com>
2015-05-08 12:46:34 +0200

    added main method

--- src/main/java/context/healthinformatics/Main.java
+++ src/main/java/context/healthinformatics/Main.java
@@ -2,11 +2,12 @@ package context.healthinformatics;
 
 import java.io.IOException;
 
+import context.healthinformatics.GUI.MainFrame;
 import context.healthinformatics.Parser.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
+		new MainFrame();
 	}
 }
