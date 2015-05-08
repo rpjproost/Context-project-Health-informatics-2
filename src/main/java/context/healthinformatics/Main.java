359d9e9e85b73a346f948e46d0076a565036f1fe
rpjproost <rickproost181@hotmail.com>
2015-05-08 12:34:29 +0200

    main modified to master's version

--- src/main/java/context/healthinformatics/Main.java
+++ src/main/java/context/healthinformatics/Main.java
@@ -1,11 +1,13 @@
 package context.healthinformatics;
 
 import java.io.IOException;
-import context.healthinformatics.GUI.MainFrame;
+
+import context.healthinformatics.GUI.GUI;
+import context.healthinformatics.Parser.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		new MainFrame();
+		GUI.main(args);
 	}
 }
