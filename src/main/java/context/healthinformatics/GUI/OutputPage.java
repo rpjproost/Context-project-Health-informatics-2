387acc809e847dad0fd0c22c46973809cf1dea6e
jelmerdeboer <J.deBoer-2@student.tudelft.nl>
2015-05-12 12:05:02 +0200

    Created a save button which will pop up a selector where you can save your file.

--- src/main/java/context/healthinformatics/GUI/OutputPage.java
+++ src/main/java/context/healthinformatics/GUI/OutputPage.java
@@ -1,8 +1,16 @@
 package context.healthinformatics.GUI;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.Serializable;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 
 /**
@@ -11,7 +19,11 @@ import javax.swing.JPanel;
 public class OutputPage implements PanelState, Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private static final int FONTSIZE = 18;
+	private static final int BUTTONWIDTH = 300;
+	private static final int BUTTONHEIGHT = 50;
 	private MainFrame mf;
+	private JButton exportFile;
 	
 	/**
 	 * Constructor.
@@ -25,8 +37,40 @@ public class OutputPage implements PanelState, Serializable {
 	public JPanel loadPanel() {
 		JPanel panel = MainFrame.createPanel(Color.decode("#086A87"),
 				mf.getScreenWidth(), mf.getStatePanelSize());
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		exportFile = createButton("Export File...");
+		panel.add(exportFile, c);
 		return panel;
 	}
 	
+	private JButton createButton(String name) {
+		JButton button = new JButton(name);
+		button.setPreferredSize(new Dimension(BUTTONWIDTH, BUTTONHEIGHT));
+		button.setFont(new Font("Arial", Font.ITALIC, FONTSIZE));
+		button.addActionListener(new ActionHandler());
+		return button;
+	}
+	
+	/**
+	 * Class which handles the actions when buttons are clicked.
+	 */
+	private class ActionHandler implements ActionListener {
+
+		/**
+		 * Action when the button is pressed the save pop up will be shown.
+		 */
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JFileChooser c = new JFileChooser();
+			if (e.getSource() == exportFile) {
+				int rVal = c.showSaveDialog(exportFile);
+			      if (rVal == JFileChooser.APPROVE_OPTION) {
+			        exportFile.setText(c.getCurrentDirectory().toString() 
+			        		+ "\\" + c.getSelectedFile().getName());
+			      }
+			}
+		}
+	}
 
 }
