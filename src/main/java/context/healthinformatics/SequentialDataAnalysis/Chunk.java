1dc646dd896dd163a04f1afbd63b04b9940900db
wimspaargaren <wim_spaargaren@live.nl>
2015-05-18 15:21:19 +0200

    Made test package sequential data analysis. Implemented getters and setters chunk.

--- src/main/java/context/healthinformatics/SequentialDataAnalysis/Chunk.java
+++ src/main/java/context/healthinformatics/SequentialDataAnalysis/Chunk.java
@@ -11,12 +11,52 @@ public class Chunk {
 	public ArrayList<Chunk> chunks;
 	public ResultSet res;
 	
-	public Chunk() {
+	public Chunk(String code) {
 		chunks = new ArrayList<Chunk>();
 		pointer = new HashMap<Chunk, String>();
 		comment = null;
-		code = null;
+		this.code = code;
 		
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public HashMap<Chunk, String> getPointer() {
+		return pointer;
+	}
+
+	public void setPointer(HashMap<Chunk, String> pointer) {
+		this.pointer = pointer;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public ArrayList<Chunk> getChunks() {
+		return chunks;
+	}
+
+	public void setChunks(ArrayList<Chunk> chunks) {
+		this.chunks = chunks;
+	}
+
+	public ResultSet getRes() {
+		return res;
+	}
+
+	public void setRes(ResultSet res) {
+		this.res = res;
 	}
 
 }
---
+++ src/test/java/context/healthinformatics/SequentialDataAnalysis/ConstraintsTest.java
@@ -0,0 +1,24 @@
+package context.healthinformatics.SequentialDataAnalysis;
+
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+/**
+ * Test for class Constraints.
+ */
+public class ConstraintsTest {
+	
+	/**
+	 * Test for code constraint.
+	 */
+	@Test
+	public void testCodeConstraints() {
+		ArrayList<Chunk> cList = new ArrayList<Chunk>();
+		
+		Constraints cs = new Constraints(cList);
+	}
+
+}
