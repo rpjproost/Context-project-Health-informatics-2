773a65ccb988294ba982526a8cf2592dcc94e346
wimspaargaren <wim_spaargaren@live.nl>
2015-05-18 15:14:12 +0200

    Implemented dummy methods Constrains.

--- src/main/java/context/healthinformatics/SequentialDataAnalysis/Constraints.java
+++ src/main/java/context/healthinformatics/SequentialDataAnalysis/Constraints.java
@@ -1,49 +1,111 @@
 package context.healthinformatics.SequentialDataAnalysis;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Class Constraints.
  */
 public class Constraints {
 	private ArrayList<Chunk> chunks;
+	private String columnName;
 
+	/**
+	 * Constructor Constraints.
+	 * 
+	 * @param chunks
+	 *            the list of chunks
+	 */
 	public Constraints(ArrayList<Chunk> chunks) {
 		this.chunks = chunks;
 	}
-	
-	//constraint code
-	//constraint comment
-	//constraint pointer
-	
-	
-	//constraint op value int
-	//constraint op value string
-	//constraint op value date
-	
-	public ArrayList<Chunk> constraintCode(String code){
-		//iterate through chunks
-		
-		//return remaining chunks
-		//TODO
+
+	/**
+	 * Constructor Constraints.
+	 * 
+	 * @param columnName
+	 *            the columnName of the value
+	 * @param chunks
+	 *            the list of chunks
+	 */
+	public Constraints(ArrayList<Chunk> chunks, String columnName) {
+		this.chunks = chunks;
+		this.columnName = columnName;
+	}
+
+	/**
+	 * Put constraint on code.
+	 * 
+	 * @param code
+	 *            the code to constraint
+	 * @return return the ArrayList with remaining chuncks
+	 */
+	public ArrayList<Chunk> constraintCode(String code) {
+		// iterate through chunks
+
+		// return remaining chunks
+		// TODO
 		return null;
 	}
-	
-	public ArrayList<Chunk> constraintComment(String comment){
-		//iterate through chunks
-		
-		//return remaining chunks
+
+	/**
+	 * Put constraint on comment.
+	 * 
+	 * @param comment
+	 *            the comment to constraint
+	 * @return return the ArrayList with remaining chuncks
+	 */
+	public ArrayList<Chunk> constraintComment(String comment) {
+		// iterate through chunks
+
+		// return remaining chunks
+		// TODO
 		return null;
 	}
-	
-	public ArrayList<Chunk> constraint(String code){
-		//iterate through chunks
-		
-		//return remaining chunks
+
+	/**
+	 * Constraint on a data value string.
+	 * 
+	 * @param value
+	 *            the string value
+	 * @return return the ArrayList with remaining chuncks
+	 */
+	public ArrayList<Chunk> constraint(String value) {
+		// iterate through chunks
+
+		// return remaining chunks
+		// TODO
 		return null;
 	}
-	
-	
-	
-	
+
+	/**
+	 * Constraint on a data value int.
+	 * 
+	 * @param value
+	 *            the int value
+	 * @return return the ArrayList with remaining chuncks
+	 */
+	public ArrayList<Chunk> constraint(int value) {
+		// iterate through chunks
+
+		// return remaining chunks
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Constraint on a data value Date.
+	 * 
+	 * @param value
+	 *            the Date value
+	 * @return return the ArrayList with remaining chuncks
+	 */
+	public ArrayList<Chunk> constraint(Date value) {
+		// iterate through chunks
+
+		// return remaining chunks
+		// TODO
+		return null;
+	}
+
 }
