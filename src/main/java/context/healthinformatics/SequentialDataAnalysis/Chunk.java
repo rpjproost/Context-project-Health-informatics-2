4344dcbd7c0bfd228e33358cbf3d0c2e17e3adfc
rpjproost <rickproost181@hotmail.com>
2015-05-18 15:33:40 +0200

    -added javadoc -changed constructor -changed getters/setters

--- src/main/java/context/healthinformatics/SequentialDataAnalysis/Chunk.java
+++ src/main/java/context/healthinformatics/SequentialDataAnalysis/Chunk.java
@@ -4,57 +4,104 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * 
+ * Class chunk.
+ *
+ */
 public class Chunk {
-	public String code;
-	public HashMap<Chunk, String> pointer;
-	public String comment;
-	public ArrayList<Chunk> chunks;
-	public ResultSet res;
-	
-	public Chunk(String code, ArrayList<Chunk> chunks) {
-		this.chunks = chunks;
+	private String code;
+	private HashMap<Chunk, String> pointer;
+	private String comment;
+	private ArrayList<Chunk> chunks;
+	private ResultSet res;
+
+	/**
+	 * Class chunk.
+	 */
+	public Chunk() {
+		chunks = new ArrayList<Chunk>();
 		pointer = new HashMap<Chunk, String>();
 		comment = null;
-		this.code = code;
-		
+		code = null;
+
 	}
 
+	/**
+	 * 
+	 * @return code for this chunk.
+	 */
 	public String getCode() {
 		return code;
 	}
 
+	/**
+	 * 
+	 * @param code set code of this chunk.
+	 */
 	public void setCode(String code) {
 		this.code = code;
 	}
 
+	/**
+	 * 
+	 * @return pointer connection to other chunk.
+	 */
 	public HashMap<Chunk, String> getPointer() {
 		return pointer;
 	}
 
+	/** set pointer.
+	 * 
+	 * @param pointer pointer where this chunk has connection to.
+	 */
 	public void setPointer(HashMap<Chunk, String> pointer) {
 		this.pointer = pointer;
 	}
 
+	/**
+	 * 
+	 * @return comment for this chunk.
+	 */
 	public String getComment() {
 		return comment;
 	}
 
+	/** Set comment.
+	 * 
+	 * @param comment comment to be set for this chunk.
+	 */
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-
+	/** List of chunks this chunk consists out of.
+	 * 
+	 * @return childs of this chunk.
+	 */
 	public ArrayList<Chunk> getChunks() {
 		return chunks;
 	}
 
-	public void setChunks(ArrayList<Chunk> chunks) {
-		this.chunks = chunks;
+	/** Set childs of this chunk.
+	 * 
+	 * @param c chunk to be added to arraylist.
+	 */
+	public void setChunks(Chunk c) {
+		chunks.add(c);
 	}
 
+	/** 
+	 * 
+	 * @return content of this chunk.
+	 */
 	public ResultSet getRes() {
 		return res;
 	}
 
+	/** Set content of this chunk.
+	 * 
+	 * @param res resultset to be added to this chunk.
+	 */
 	public void setRes(ResultSet res) {
 		this.res = res;
 	}
