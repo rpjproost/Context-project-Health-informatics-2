d81bff833b069e988070ef28cc95cd52600849a6
rpjproost <rickproost181@hotmail.com>
2015-05-18 15:09:31 +0200

    initial chunk class

--- src/main/java/context/healthinformatics/SequentialDataAnalysis/Chunk.java
+++ src/main/java/context/healthinformatics/SequentialDataAnalysis/Chunk.java
@@ -1,5 +1,22 @@
 package context.healthinformatics.SequentialDataAnalysis;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Chunk {
+	public String code;
+	public HashMap<Chunk, String> pointer;
+	public String comment;
+	public ArrayList<Chunk> chunks;
+	public ResultSet res;
+	
+	public Chunk() {
+		chunks = new ArrayList<Chunk>();
+		pointer = new HashMap<Chunk, String>();
+		comment = null;
+		code = null;
+		
+	}
 
 }
