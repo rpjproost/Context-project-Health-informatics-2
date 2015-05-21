fe81c2779ec4b8ba488fa8e31ba0e69edef95825
rpjproost <rickproost181@hotmail.com>
2015-05-21 11:55:32 +0200

    -First test for mergeTables

--- src/test/java/context/healthinformatics/Database/MergeTableTest.java
+++ src/test/java/context/healthinformatics/Database/MergeTableTest.java
@@ -41,6 +41,35 @@ public class MergeTableTest {
 			System.out.println("Something went wrong preparing db for tests.");
 		}
 	}
+	
+	/**
+	 * Test for merging two tables with condition.
+	 * @throws IOException if files could not be read.
+	 * @throws SQLException if tables could not be created.
+	 */
+	@Test
+	public void mergeTableTest() throws IOException, SQLException {
+		xmlp = new XMLParser(path + "twoDocs.xml");
+		xmlp.parse();
+		final int res = 209;
+		String[] clause = new String[1];
+		clause[0] = "StatSensor.value = 209";
+		MergeTable test = new MergeTable();
+		test.mergeTables(clause);
+		ResultSet rs = data.selectResultSet("result", "value");
+		while (rs.next()) {
+			if (rs.getInt("value") == 0) {
+				assertEquals((rs.getInt("value")), 0);
+			}
+			else {
+				assertEquals((rs.getInt("value")), res);
+			}
+		}
+		rs.close();
+		data.dropTable("result");
+		data.dropTable("HospitalRecords");
+		data.dropTable("StatSensor");
+	}
 
 
 }
