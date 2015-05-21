440847bfbf706deb4870cd57a935d536a0fd32e7
wimspaargaren <wim_spaargaren@live.nl>
2015-05-21 11:08:03 +0200

    Setup Comments class. Setup of Comments Test class.

--- src/main/java/context/healthinformatics/SequentialDataAnalysis/Comments.java
+++ src/main/java/context/healthinformatics/SequentialDataAnalysis/Comments.java
@@ -1,8 +1,39 @@
 package context.healthinformatics.SequentialDataAnalysis;
 
+import java.util.ArrayList;
+
 /**
  * The Class Comments.
  */
 public class Comments {
 
+	private ArrayList<Chunk> chunks;
+
+	/**
+	 * Constructor for comments.
+	 * 
+	 * @param chunks
+	 *            the chunks
+	 */
+	public Comments(ArrayList<Chunk> chunks) {
+		this.chunks = chunks;
+	}
+
+	public void setComment(int line, String comment) {
+		getChunk(line, this.chunks, comment);
+
+	}
+
+	public Chunk getChunk(int line, ArrayList<Chunk> chunk, String comment) {
+		for (int i = 0; i < chunk.size(); i++) {
+			Chunk curChunk = chunk.get(i);
+			if (curChunk.getLine() == line) {
+				curChunk.setComment(comment);
+				return curChunk;
+			} else if (curChunk.hasChild()) {
+				getChunk(line, curChunk.getChunks(), comment);
+			}
+		}
+		return null;
+	}
 }
--- src/test/java/context/healthinformatics/SequentialDataAnalysis/CommentsTest.java
+++ src/test/java/context/healthinformatics/SequentialDataAnalysis/CommentsTest.java
@@ -1,8 +1,54 @@
 package context.healthinformatics.SequentialDataAnalysis;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Tests for class Comments.
  */
 public class CommentsTest {
+	/**
+	 * The constrains class.
+	 */
+	private Comments c;
 
+	/**
+	 * The list of chunks.
+	 */
+	private ArrayList<Chunk> cList2;
+
+	private static final int THREE = 3;
+
+	/**
+	 * Setup a list of chunks.
+	 */
+	@Before
+	public void before() {
+		ArrayList<Chunk> cList = new ArrayList<Chunk>();
+		Chunk c1 = new Chunk();
+		c1.setLine(1);
+		cList.add(c1);
+		Chunk c2 = new Chunk();
+		c2.setLine(2);
+		cList.add(c2);
+		Chunk c3 = new Chunk();
+		c3.setChunks(cList);
+		Chunk c4 = new Chunk();
+		c4.setLine(THREE);
+		cList2 = new ArrayList<Chunk>();
+		cList2.add(c3);
+		cList2.add(c4);
+		c = new Comments(cList2);
+	}
+
+	/**
+	 * Test is a comment is set.
+	 */
+	@Test
+	public void testSetComment() {
+		c.setComment(3, "hallo");
+		System.out.println(cList2);
+	}
 }
