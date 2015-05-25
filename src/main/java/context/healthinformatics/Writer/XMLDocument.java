303d3960ce6675009e4ebd4523752dc6471ab974
wimspaargaren <wim_spaargaren@live.nl>
2015-05-25 13:01:58 +0200

    Adjusted Constructor to get a doctype. Added the getters.

--- src/main/java/context/healthinformatics/Writer/XMLDocument.java
+++ src/main/java/context/healthinformatics/Writer/XMLDocument.java
@@ -1,16 +1,13 @@
 package context.healthinformatics.Writer;
 
 import java.util.ArrayList;
-
-import org.w3c.dom.Document;
-
 import context.healthinformatics.Parser.Column;
 
 /**
  * Class document to store the info of a document in the XML file.
  */
 public class XMLDocument {
-	private Document doc;
+	private String docType;
 	private String docName;
 	private String delimiter;
 	private String path;
@@ -20,26 +17,96 @@ public class XMLDocument {
 
 	/**
 	 * Constructor for the XML document.
-	 * @param doc the document
-	 * @param docName the name of the document
-	 * @param delimiter the delimiter
-	 * @param path the path to the document
-	 * @param start the start  variable
-	 * @param columns the list of columns
+	 * 
+	 * @param docType
+	 *            the type of the document
+	 * @param docName
+	 *            the name of the document
+	 * @param delimiter
+	 *            the delimiter
+	 * @param path
+	 *            the path to the document
+	 * @param start
+	 *            the start variable
+	 * @param columns
+	 *            the list of columns
 	 */
-	public XMLDocument(Document doc, String docName, String delimiter,
+	public XMLDocument(String docType, String docName, String delimiter,
 			String path, int start, ArrayList<Column> columns) {
-		if (doc.equals("excel")) {
+		if (docType.equals("excel")) {
 			this.sheet = start;
 			this.startLine = -1;
 		} else {
 			this.startLine = start;
 			this.sheet = -1;
 		}
-		this.doc = doc;
+		this.docType = docType;
 		this.docName = docName;
 		this.delimiter = delimiter;
 		this.columns = columns;
+	}
+
+	/**
+	 * Get the type of the document.
+	 * 
+	 * @return the document type
+	 */
+	public String getDocType() {
+		return docType;
+	}
+
+	/**
+	 * get the name of the document.
+	 * 
+	 * @return the name of the document
+	 */
+	public String getDocName() {
+		return docName;
+	}
+
+	/**
+	 * Get the delimiter of the document.
+	 * 
+	 * @return the delimiter of the document
+	 */
+	public String getDelimiter() {
+		return delimiter;
+	}
+
+	/**
+	 * Get the path of where the file is stored.
+	 * 
+	 * @return the path of the file
+	 */
+	public String getPath() {
+		return path;
+	}
+
+	/**
+	 * Get the starting line of the document.
+	 * 
+	 * @return the start line
+	 */
+	public int getStartLine() {
+		return startLine;
+	}
+
+	/**
+	 * Get the start sheet of the document.
+	 * 
+	 * @return the sheet of the document
+	 */
+	public int getSheet() {
+		return sheet;
+	}
+
+	/**
+	 * Get the list of columns.
+	 * 
+	 * @return the list of columns
+	 */
+	public ArrayList<Column> getColumns() {
+		return columns;
 	}
 
 }
