9b71eccfde4d82deb540b912859a9372a5bdc6d8
pascalremeijsen <P.Remeijsen@student.tudelft.nl>
2015-05-25 14:15:54 +0200

    Created clss for connections.

---
+++ src/main/java/context/healthinformatics/SequentialDataAnalysis/Connections.java
@@ -0,0 +1,21 @@
+package context.healthinformatics.SequentialDataAnalysis;
+
+import java.util.ArrayList;
+
+/**
+ * The Class Connections.
+ */
+public class Connections {
+
+	private ArrayList<Chunk> chunks;
+
+	/**
+	 * Constructor for connections.
+	 * 
+	 * @param chunks
+	 *            the chunks
+	 */
+	public Connections(ArrayList<Chunk> chunks) {
+		this.chunks = chunks;
+	}
+}
