27fa476b4a5f2512ebf5764749ddf059656e9131
pascalremeijsen <P.Remeijsen@student.tudelft.nl>
2015-06-03 09:51:30 +0200

    Codes now implemnts Tasks, and has second (empty) constructor.

--- src/main/java/context/healthinformatics/sequentialdataanalysis/Codes.java
+++ src/main/java/context/healthinformatics/sequentialdataanalysis/Codes.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 /**
  * The Class Codes.
  */
-public class Codes {
+public class Codes extends Tasks {
 
 	private ArrayList<Chunk> chunks;
+	
+	/**
+	 * Constructor for codes without an argument.
+	 */
+	public Codes() { }
 
 	/**
 	 * Constructor for codes.
@@ -17,6 +22,18 @@ public class Codes {
 	 */
 	public Codes(ArrayList<Chunk> chunks) {
 		this.chunks = chunks;
+	}
+	
+	@Override
+	public ArrayList<Chunk> undo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	/**
@@ -32,6 +49,25 @@ public class Codes {
 	public void setCodeOfLine(int line, String code) throws Exception {
 		try {
 			getChunk(line).setCode(code);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
+	/**
+	 * Set the code on the chunk at index in the chunks ArrayList with the
+	 * string code.
+	 * 
+	 * @param index
+	 *            the index of the chunk in the ArrayList chunks
+	 * @param code
+	 *            the code to be set
+	 * @throws Exception
+	 */
+	public void setCodeOfChunks(int index, String code) {
+		try {
+			Chunk c = chunks.get(index);
+			c.setCode(code);
 		} catch (Exception e) {
 			throw e;
 		}
@@ -56,24 +92,4 @@ public class Codes {
 		}
 		throw new Exception();
 	}
-
-	/**
-	 * Set the code on the chunk at index in the chunks ArrayList with the
-	 * string code.
-	 * 
-	 * @param index
-	 *            the index of the chunk in the ArrayList chunks
-	 * @param code
-	 *            the code to be set
-	 * @throws Exception
-	 */
-	public void setCodeOfChunks(int index, String code) {
-		try {
-			Chunk c = chunks.get(index);
-			c.setCode(code);
-		} catch (Exception e) {
-			throw e;
-		}
-	}
-
 }
