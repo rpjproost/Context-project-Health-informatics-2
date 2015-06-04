873cbe6bbf83b4b8be6a6e82ee2ef3ad1d987954
jelmerdeboer <J.deBoer-2@student.tudelft.nl>
2015-06-04 17:01:15 +0200

    Created the left side interface of the analyse tab

--- src/main/java/context/healthinformatics/gui/CodePage.java
+++ src/main/java/context/healthinformatics/gui/CodePage.java
@@ -11,7 +11,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -33,18 +32,21 @@ public class CodePage extends InterfaceHelper implements PanelState,
 		Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private static final int FIELDCORRECTION = 200;
-	private static final int ANALYZEBUTTONWIDTH = 200;
-	private static final int ANALYZEBUTTONHEIGHT = 100;
+	private static final int FIELDCORRECTION = 130;
+	private static final int ANALYZEBUTTONWIDTH = 220;
+	private static final int ANALYZEBUTTONHEIGHT = 80;
 	private static final int INSETS = 10;
+	private static final int THREE = 3;
+	private static final int FOUR = 4;
 	private MainFrame mf;
 	private JTextArea codeTextArea;
 	private JTextArea intermediateResult;
-	private JButton button;
 	private Interpreter interpreter = SingletonInterpreter.getInterpreter();
 	private JPanel codePageParentpanel;
 	private JPanel leftPanel;
 	private JPanel rightPanel;
+	private JButton analyseButton;
+	private JButton goBackButton;
 
 	/**
 	 * Constructor.
@@ -61,7 +63,7 @@ public class CodePage extends InterfaceHelper implements PanelState,
 	@Override
 	public JPanel loadPanel() {
 		mergeTables();
-		codePageParentpanel = MainFrame.createPanel(MainFrame.CODETABCOLOR,
+		codePageParentpanel = createPanel(MainFrame.CODETABCOLOR,
 				mf.getScreenWidth(), mf.getStatePanelSize());
 		codePageParentpanel.setLayout(new GridBagLayout());
 
@@ -84,18 +86,21 @@ public class CodePage extends InterfaceHelper implements PanelState,
 		}
 	}
 
+	/**
+	 * Creates the left side of the analyze tab interface.
+	 */
 	private void setLeftPanelWithCodeField() {
 		setCodeInputAreaTitle();
 		setCodeInputArea();
+		setHelpAreaTitle();
 		setHelpArea();
-		button = createButton("Analyse", ANALYZEBUTTONWIDTH,
-				ANALYZEBUTTONHEIGHT);
-		button.addActionListener(new ActionHandler());
-		leftPanel.add(button, setGrids(0, 3));
-
+		setButtonArea();
 		codePageParentpanel.add(leftPanel, setGrids(0, 0));
 	}
 
+	/**
+	 * Sets a title to the code input area panel.
+	 */
 	private void setCodeInputAreaTitle() {
 		JLabel codeTextTitleLabel = createTitle("Code input area:");
 		GridBagConstraints c = setGrids(0, 0);
@@ -104,6 +109,9 @@ public class CodePage extends InterfaceHelper implements PanelState,
 		leftPanel.add(codeTextTitleLabel, c);
 	}
 	
+	/**
+	 * Sets the code input area.
+	 */
 	private void setCodeInputArea() {
 		codeTextArea = createTextField(((mf.getScreenWidth() / 2) - (2 * INSETS)),
 				mf.getStatePanelSize() / 2 - FIELDCORRECTION);
@@ -112,9 +120,23 @@ public class CodePage extends InterfaceHelper implements PanelState,
 		leftPanel.add(codeTextArea, c);
 	}
 	
+	/**
+	 * Sets the help area title.
+	 */
+	private void setHelpAreaTitle() {
+		JLabel helpAreaTitle = createTitle("Help");
+		GridBagConstraints c = setGrids(0, 2);
+		c.anchor = GridBagConstraints.LINE_START;
+		c.insets = new Insets(0, INSETS, 0, 0);
+		leftPanel.add(helpAreaTitle, c);
+	}
+	
+	/**
+	 * Sets the help area.
+	 */
 	private void setHelpArea() {
 		ReadHelpInfoFromTXTFile test = new ReadHelpInfoFromTXTFile(
-				"src/main/data/guihelpdata/inputpagehelp.txt");
+				"src/main/data/guihelpdata/codepagehelp.txt");
 		try {
 			test.parse();
 		} catch (IOException e) {
@@ -122,10 +144,64 @@ public class CodePage extends InterfaceHelper implements PanelState,
 		}
 		ArrayList<HelpFrameInfoContainer> listOfHelpFrameInfo = test
 				.getHelpFrameInfoContainer();
-		HelpFrame helpFrame = new HelpFrame("Input Page Help", listOfHelpFrameInfo);
-		leftPanel.add(helpFrame.getJPanel(), setGrids(0, 2));
+		HelpFrame helpFrame = new HelpFrame("Input Page Help", 
+				listOfHelpFrameInfo, ((mf.getScreenWidth() / 2) - (2 * INSETS)),
+				mf.getStatePanelSize() / 2 - FIELDCORRECTION);
+		leftPanel.add(helpFrame.getJPanel(), setGrids(0, THREE));
+	}
+	
+	/**
+	 * Sets up the whole button area.
+	 */
+	private void setButtonArea() {
+		JPanel buttonArea = createPanel(MainFrame.CODETABCOLOR, 
+				mf.getScreenWidth() / 2, FIELDCORRECTION);
+		setGoBackButton(buttonArea);
+		setEmptyPanel(buttonArea);
+		setAnalyseButton(buttonArea);
+		leftPanel.add(buttonArea, setGrids(0, FOUR));
 	}
 
+	/**
+	 * Sets the go back button to a specific panel.
+	 * @param panel the panel which the go back button will added to.
+	 */
+	private void setGoBackButton(JPanel panel) {
+		goBackButton = createButton("Go Back", ANALYZEBUTTONWIDTH,
+				ANALYZEBUTTONHEIGHT);
+		goBackButton.addActionListener(new ActionHandler());
+		GridBagConstraints c = setGrids(0, 0);
+		c.insets = new Insets(INSETS, INSETS, INSETS, 0);
+		panel.add(goBackButton, c);
+	}
+	
+	/**
+	 * Sets empty panel between the buttons.
+	 * @param panel which the empty panel will be added to.
+	 */
+	private void setEmptyPanel(JPanel panel) {
+		JPanel emptyPanel = createPanel(MainFrame.CODETABCOLOR, 
+				(mf.getScreenWidth() / 2) - (2 * ANALYZEBUTTONWIDTH) - (2 * INSETS), 
+				FIELDCORRECTION);
+		panel.add(emptyPanel, setGrids(1, 0));
+	}
+	
+	/**
+	 * Sets a analyze button to the specific panel.
+	 * @param panel which the analyze button will be added to.
+	 */
+	private void setAnalyseButton(JPanel panel) {
+		analyseButton = createButton("Analyse", ANALYZEBUTTONWIDTH,
+				ANALYZEBUTTONHEIGHT);
+		analyseButton.addActionListener(new ActionHandler());
+		GridBagConstraints c = setGrids(2, 0);
+		c.insets = new Insets(INSETS, 0, INSETS, INSETS);
+		panel.add(analyseButton, c);
+	}
+
+	/**
+	 * Sets the right side of the interface with specific panels.
+	 */
 	private void setRightPanelWithIntermediateResult() {
 		IntermediateResults imr = new IntermediateResults(mf);
 		rightPanel.add(imr.loadPanel(), setGrids(0, 0));
