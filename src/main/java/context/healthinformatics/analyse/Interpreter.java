3e5796bbdf2d1876dcb206c3f90992ff28c4d545
ArnoutvdKnaap <ArnoutvdKnaap@live.nl>
2015-06-08 14:31:04 +0200

    added a separate query class. removed and changed methods of interpreter that where either unneeded or in Query

--- src/main/java/context/healthinformatics/analyse/Interpreter.java
+++ src/main/java/context/healthinformatics/analyse/Interpreter.java
@@ -44,41 +44,19 @@ public class Interpreter {
 		Scanner sc = new Scanner(code);
 		
 		while (sc.hasNextLine()) {
-			ArrayList<String> strings = new ArrayList<String>();
-			String parameter = splitLine(strings, sc);
-			strings = checkSplittedLineForUnwantedSpaces(strings);
-			Task task = createTask(strings.get(0), parameter);
+			Query query = new Query(sc.nextLine());
+			Task task = createTask(query);
 			if (task != null) { //task == null if revert / undo was called
-				task.run(buildStringArray(strings));
+				task.run(query);
 				tasks.push(task);
-			} else if (strings.get(0).equals("undo") || strings.get(0).equals("revert")) {
+			} else if (query.part().equals("undo") || query.part().equals("revert")) {
 				undo();
 			}
 		}
 		sc.close();
 	}
 	
-	/**
-	 * Method to handle the splitting of the line.
-	 * @param a new list for the splitted strings.
-	 * @return the parameter found.
-	 * @throws Exception Might throw an index out of bounds exception.
-	 */
-	private String splitLine(ArrayList<String> strings, Scanner sc) throws Exception {
-		String[] splittedParameterLine = splitParameter(sc.nextLine());
-		String parameter = null;
-		addStrings(splittedParameterLine[0].split(" "), strings);
-		try {
-			if (splittedParameterLine.length > 1) {
-				addStrings(splittedParameterLine[2].split(" "), strings);
-				parameter = splittedParameterLine[1];
-			}
-		} catch (Exception e) {
-			sc.close();
-			throw new Exception(e.getMessage());
-		}
-		return parameter;
-	}
+	
 	
 	/**
 	 * method to create the correct task.
@@ -130,20 +108,6 @@ public class Interpreter {
 		
 	}
 	
-	/**
-	 * checks the splitted line for empty strings in case of dubble " ".
-	 * @param splittedLine ArrayList of strings with the splitted line.
-	 * @return A list of strings without empty places.
-	 */
-	private ArrayList<String> checkSplittedLineForUnwantedSpaces(ArrayList<String> splittedLine) {
-		ArrayList<String> strings = new ArrayList<String>();
-		for (String split : splittedLine) {
-			if (split.length() > 0) {
-				strings.add(split);
-			}
-		}
-		return strings;
-	}
 	
 	/**
 	 * setter for the initial list of chunks.
@@ -153,48 +117,7 @@ public class Interpreter {
 		firstList = list;
 	}
 	
-	/**
-	 * split the line to get the parameter out.
-	 * @param line line to split
-	 * @return the splitted line;
-	 */
-	private String[] splitParameter(String line) {
-		ArrayList<String> strings = new ArrayList<String>();
-		int fp = findChar(line, "(");
-		int sp = findChar(line, ")");
-		if (fp == -1) { //geen haakjes
-			strings.add(line);
-			return buildStringArray(strings);
-		}
-		strings.add(line.substring(0, fp));
-		strings.add(line.substring(fp + 1, sp));
-		strings.add(line.substring(sp + 1, line.length()));
-		return buildStringArray(strings);
-	}
-	
-	/**
-	 * builds an array from strings from an ArrayList of Strings.
-	 * @param strings arrayList to build from.
-	 * @return the created String array.
-	 */
-	private String[] buildStringArray(ArrayList<String> strings) {
-		String[] ans  = new String[strings.size()];
-		for (int i = 0; i < strings.size(); i++) {
-			ans[i] = strings.get(i);
-		}
-		return ans;
-	}
-	
-	/**
-	 * adds all strings from the string array to the list.
-	 * @param array array of strings to add
-	 * @param list list to add the strings to.
-	 */
-	private void addStrings(String[] array, ArrayList<String> list) {
-		for (int i = 0; i < array.length; i++) {
-			list.add(array[i]);
-		}
-	}
+
 	
 	/**
 	 * deep coppy a list of chunks.
@@ -218,14 +141,6 @@ public class Interpreter {
 		}
 	}
 	
-	private int findChar(String string, String cha) {
-		for (int i = 0; i < string.length(); i++) {
-			if (string.charAt(i) == cha.charAt(0)) {
-				return i;
-			}
-		}
-		return -1;
-	}
 	
 	/**
 	 * getter for the name of the interpreter.
---
+++ src/main/java/context/healthinformatics/analyse/Query.java
@@ -0,0 +1,129 @@
+package context.healthinformatics.analyse;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * class containing the user query for parsing.
+ */
+public class Query {
+	private ArrayList<String> query;
+	private String parameter;
+	private int index;
+	
+	/**
+	 * Constructor for a query.
+	 * @param q String with the query.
+	 */
+	public Query(String q) {
+		index = 0;
+		parameter = "";
+		String line = splitParameter(q);
+		query = splitLine(line);
+	}
+	
+	
+	/**
+	 * Method to handle the splitting of the line.
+	 * @param a new list for the splitted strings.
+	 * @return the parameter found.
+	 * @throws Exception Might throw an index out of bounds exception.
+	 */
+	private ArrayList<String> splitLine(String inputLine) {
+		String line = splitParameter(inputLine);
+		ArrayList<String> parts = new ArrayList<String>();
+		addStrings(line.split(" "), parts);
+		checkSplittedLineForUnwantedSpaces(parts);
+		return parts;
+	}
+	
+	/**
+	 * checks the splitted line for empty strings in case of dubble " ".
+	 * @param splittedLine ArrayList of strings with the splitted line.
+	 * @return A list of strings without empty places.
+	 */
+	private void checkSplittedLineForUnwantedSpaces(ArrayList<String> splittedLine) {
+		int[] remove = new int[splittedLine.size()];
+		int count = 0;
+		for (int i = 0; i < splittedLine.size(); i++) {
+			if (!(splittedLine.get(i).length() > 0)) {
+				remove[count] = i;
+				count++;
+			}
+		}
+		for (int i = 0; i < count; i++) {
+			splittedLine.remove(remove[count - i]);
+		}
+	}
+	
+	/**
+	 * split the line to get the parameter out.
+	 * @param line line to split
+	 * @return the line without the parameter, parameter is now set.;
+	 */
+	private String splitParameter(String line) {
+		int fp = findChar(line, "(");
+		int sp = findChar(line, ")");
+		if (fp != -1 && checkForCommand(line.substring(0, fp))) { //has parameter
+			parameter = line.substring(fp + 1, sp);
+			return line.substring(0, fp) + line.substring(sp + 1, line.length());
+		} else {
+			return line;
+		}
+	}
+	
+	
+	/**
+	 * adds all strings from the string array to the list.
+	 * @param array array of strings to add
+	 * @param list list to add the strings to.
+	 */
+	private void addStrings(String[] array, ArrayList<String> list) {
+		for (int i = 0; i < array.length; i++) {
+			list.add(array[i]);
+		}
+	}
+	
+	/**
+	 * find char in a string.
+	 * @param string string to find for
+	 * @param cha character (first cha of string)
+	 * @return the location of the cha in the string.
+	 */
+	private int findChar(String string, String cha) {
+		for (int i = 0; i < string.length(); i++) {
+			if (string.charAt(i) == cha.charAt(0)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * checks if the string given is 1 word.
+	 * @param s given string
+	 * @return boolean containing the answer. True for 1 word. False for more then 1 word.
+	 */
+	private boolean checkForCommand(String s) { //checks if the string is 1 word.
+		return Pattern.matches("/\\s*\b([A-Z]*[a-z]+)+\\s*\b/g", s);
+	}
+	
+	/**
+	 * set the queryPointer to the int.
+	 * does nothing if outOfBounds.
+	 * @param i int to point to.
+	 */
+	public void setPointer(int i) {
+		if (i >= 0 && i < query.size()) {
+			index = i;
+		}
+	}
+	/**
+	 * returns the current queryPart.
+	 * @return string containing the current query part.
+	 */
+	public String part() {
+		return query.get(index);
+	}
+}
