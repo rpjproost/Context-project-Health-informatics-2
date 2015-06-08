2fb719b6d59da83f28af4c98fe72607e647a86d3
jelmerdeboer <J.deBoer-2@student.tudelft.nl>
2015-06-08 13:39:39 +0200

    Expanded the tests of the xml controller

--- src/test/java/context/healthinformatics/interfacecomponents/XMLEditorControllerTest.java
+++ src/test/java/context/healthinformatics/interfacecomponents/XMLEditorControllerTest.java
@@ -1,13 +1,20 @@
 package context.healthinformatics.interfacecomponents;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import context.healthinformatics.gui.InputPage;
 import context.healthinformatics.parser.Column;
 import context.healthinformatics.writer.XMLDocument;
 
@@ -19,23 +26,29 @@ public class XMLEditorControllerTest {
 	private XMLDocument documentOne;
 	private XMLDocument documentTwo;
 	private XMLEditorController controller;
-	private String project = "ADMIRE";
+	private String project = "testingXMLController";
+	private ArrayList<XMLDocument> docs;
+	private XMLEditor edit;
+	private InputPage ip;
 	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() {
+		edit = mock(XMLEditor.class);
+		ip = mock(InputPage.class);
 		Column column = new Column(1, "comment", "String");
 		ArrayList<Column> columns = new ArrayList<Column>();
 		columns.add(column);
 		documentOne = new XMLDocument("excel", "main", ";", "src/main", 1, 2, columns);
 		documentTwo = new XMLDocument("text", "test", ",", "src/test", 2, 1, columns);
-		ArrayList<XMLDocument> docs = new ArrayList<XMLDocument>();
+		docs = new ArrayList<XMLDocument>();
 		docs.add(documentOne);
 		controller = new XMLEditorController();
 		controller.setProject(project);
 		controller.setDocumentsInProject(docs);
+		when(ip.findFolderProject(project)).thenReturn(-1);
 	}
 
 	/**
@@ -126,5 +139,121 @@ public class XMLEditorControllerTest {
 	public void testGetDocumentWithPartofPathWrong() {
 		assertNull(controller.getDocumentWithPartofPath("test"));
 	}
-
+	
+	/**
+	 * Tests the get file location if it is correct.
+	 */
+	@Test
+	public void testGetFileLocation() {
+		String actual = controller.getFileLocation();
+		String expected = "src/main/data/savedXML/" + project + ".xml";
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Tests the get project documents method.
+	 */
+	@Test
+	public void testGetProjectDocuments() {
+		ArrayList<XMLDocument> actual = controller.getProjectDocuments();
+		ArrayList<XMLDocument> expected = new ArrayList<XMLDocument>();
+		expected.add(documentOne);
+		assertEquals(expected.size(), actual.size());
+		for (int i = 0; i < expected.size(); i++) {
+			assertEquals(expected.get(i).getPath(), actual.get(i).getPath());
+			assertEquals(expected.get(i).getDocName(), actual.get(i).getDocName());
+		}
+	}
+	
+	/**
+	 * Tests the break down xml docs into names.
+	 */
+	@Test
+	public void testBreakDownXMLDocumentsIntoNames() {
+		ArrayList<String> actual = controller.breakDownXMLDocumentsIntoNames(docs);
+		ArrayList<String> expected = new ArrayList<String>();
+		expected.add(project);
+		expected.add("main");
+		for (int i = 0; i < actual.size(); i++) {
+			assertEquals(expected.get(i), actual.get(i));
+		}
+	}
+	
+	/**
+	 * Tests save and remove methods.
+	 */
+	@Test
+	public void testSaveAndRemoveCorrect() {
+		controller.save();
+		File test = new File(controller.getFileLocation());
+		assertTrue(test.exists());
+		controller.removeProject(project);
+		assertFalse(test.exists());
+	}
+	
+	/**
+	 * Tests if the project will be not deleted if the file can't be deleted.
+	 */
+	@Test
+	public void testRemoveIfNoFileExists() {
+		assertEquals(1, controller.getProjectDocuments().size());
+		controller.removeProject(project);
+		assertEquals(1, controller.getProjectDocuments().size());
+	}
+	
+	/**
+	 * Tests the get indexes of selected files correctly.
+	 */
+	@Test
+	public void testGetIndexesOfSelectedFiles() {
+		testSelectDocumentCorrect();
+		ArrayList<Integer> actual = controller.getIndexesOfSelectedFiles(docs);
+		assertEquals(1, actual.size());
+	}
+	
+	/**
+	 * Tests if there is no equality between the project file and selected files.
+	 */
+	@Test
+	public void testGetIndexesOfSelectedFilesNull() {
+		testSelectDocumentCorrect();
+		ArrayList<XMLDocument> test = new ArrayList<XMLDocument>();
+		test.add(documentTwo);
+		ArrayList<Integer> actual = controller.getIndexesOfSelectedFiles(test);
+		assertEquals(0, actual.size());
+	}
+	
+	/**
+	 * Tests some null catches.
+	 */
+	@Test
+	public void testSomeNullCatches() {
+		controller.save();
+		controller.removeProject(project);
+		controller.addDocument(documentTwo);
+		assertEquals(1, controller.getProjectDocuments().size());
+		controller.setProject("ADMIRE");
+		assertNull(controller.getDocument("src/main"));
+		assertNull(controller.getDocumentWithPartofPath("main"));
+	}
+	
+	/**
+	 * Verifies if some methods will be run when you call load project.
+	 */
+	@Test
+	public void testLoadProject() {
+		controller.loadProject(edit);
+		verify(edit).emptyEditor();
+		verify(edit).addXMLDocumentToContainerScrollPanel(documentOne);
+	}
+	
+	/**
+	 * Tests if the find folder project will be called.
+	 */
+	@Test
+	public void testUpdateProject() {
+		controller.updateDocuments(ip, docs);
+		verify(ip).findFolderProject(project);
+	}
+	
 }
