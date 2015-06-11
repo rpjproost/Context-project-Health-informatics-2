8ad6e3802b6f411fac09bd8b641537b7b421aaca
wimspaargaren <wim.spaargaren@lastmason.com>
2015-06-11 12:47:50 +0200

    BoxPlotWorking

--- src/main/java/context/healthinformatics/graphs/BoxPlot.java
+++ src/main/java/context/healthinformatics/graphs/BoxPlot.java
@@ -1,33 +1,22 @@
 package context.healthinformatics.graphs;
 
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map.Entry;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.labels.BoxAndWhiskerToolTipGenerator;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.renderer.category.BoxAndWhiskerRenderer;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.statistics.BoxAndWhiskerCategoryDataset;
 import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
-import org.jfree.util.Log;
-import org.jfree.util.LogContext;
 
 import context.healthinformatics.analyse.Interpreter;
 import context.healthinformatics.analyse.SingletonInterpreter;
@@ -37,15 +26,18 @@ import context.healthinformatics.gui.InterfaceHelper;
 import context.healthinformatics.sequentialdataanalysis.Chunk;
 
 /**
- * Demonstration of a box-and-whisker chart using a {@link CategoryPlot}.
+ * Class to create the boxplot.
  */
-public class BoxPlot extends InterfaceHelper implements ActionListener {
+public class BoxPlot extends InterfaceHelper {
 
-	private ChartPanel chartPanel;
+	private static final long serialVersionUID = 1L;
+	private JPanel chartContainerPanel;
 	private JPanel mainPanel;
-	private static final int BOX_PLOT_WIDTH = 455;
-	private static final int BOX_PLOT_HEIGHT = 600;
+	private static final int BOX_PLOT_PANEL_HEIGHT = 400;
+	private static final int BOX_PLOT_HEIGHT = 300;
 	private JButton boxPlotTempRefreshButton;
+
+	private int width;
 
 	private DefaultBoxAndWhiskerCategoryDataset dataset;
 
@@ -56,11 +48,14 @@ public class BoxPlot extends InterfaceHelper implements ActionListener {
 	 *            the frame title.
 	 */
 	public BoxPlot(final String title) {
-		boxPlotTempRefreshButton = new JButton("Refresh BoxPlotWithData");
-		boxPlotTempRefreshButton.addActionListener(this);
-		mainPanel = createEmptyWithGridBagLayoutPanel();
-		mainPanel.setPreferredSize(new java.awt.Dimension(BOX_PLOT_WIDTH,
+		chartContainerPanel = createEmptyWithGridBagLayoutPanel();
+		chartContainerPanel.setPreferredSize(new Dimension(width,
 				BOX_PLOT_HEIGHT));
+		width = getScreenWidth() / 2 - 2 * INSETS;
+		boxPlotTempRefreshButton = new JButton("Refresh BoxPlotWithData");
+		mainPanel = createEmptyWithGridBagLayoutPanel();
+		mainPanel.setPreferredSize(new Dimension(width, BOX_PLOT_PANEL_HEIGHT));
+		mainPanel.add(chartContainerPanel, setGrids(0, 0));
 		mainPanel.add(boxPlotTempRefreshButton, setGrids(0, 1));
 	}
 
@@ -75,23 +70,31 @@ public class BoxPlot extends InterfaceHelper implements ActionListener {
 
 	/**
 	 * Create a botplot.
+	 * 
+	 * @param title
+	 *            the title of the boxplot
 	 */
-	public void createBoxPlot() {
+	public void createBoxPlot(String title) {
 
 		final CategoryAxis xAxis = new CategoryAxis("Type");
 		final NumberAxis yAxis = new NumberAxis("Value");
 		yAxis.setAutoRangeIncludesZero(false);
 		final BoxAndWhiskerRenderer renderer = new BoxAndWhiskerRenderer();
 		renderer.setFillBox(false);
-		renderer.setToolTipGenerator(new BoxAndWhiskerToolTipGenerator());
 		final CategoryPlot plot = new CategoryPlot(dataset, xAxis, yAxis,
 				renderer);
 
-		final JFreeChart chart = new JFreeChart("Box-and-Whisker Demo",
-				new Font("SansSerif", Font.BOLD, 14), plot, true);
-		chartPanel = new ChartPanel(chart);
-		chartPanel.setPreferredSize(new java.awt.Dimension(450, 270));
-		mainPanel.add(chartPanel, setGrids(0, 0));
+		final JFreeChart chart = new JFreeChart(title, new Font("SansSerif",
+				Font.BOLD, 14), plot, true);
+		mainPanel.remove(chartContainerPanel);
+		chartContainerPanel = createEmptyWithGridBagLayoutPanel();
+		chartContainerPanel.setPreferredSize(new Dimension(width,
+				BOX_PLOT_HEIGHT));
+		ChartPanel chartPanelTest = new ChartPanel(chart);
+		chartPanelTest.setPreferredSize(new Dimension(width, BOX_PLOT_HEIGHT));
+		chartContainerPanel.add(chartPanelTest, setGrids(0, 0));
+
+		mainPanel.add(chartContainerPanel, setGrids(0, 0));
 		mainPanel.revalidate();
 	}
 
@@ -100,113 +103,120 @@ public class BoxPlot extends InterfaceHelper implements ActionListener {
 	 * 
 	 * @param columns
 	 *            the columns for which the box plot is made
-	 * @throws Exception
-	 *             if the dtaa of the columns is not found throw exception
 	 */
-	public void setData(ArrayList<String> columns) throws Exception {
+	public void setDataPerColumn(ArrayList<String> columns) {
 		dataset = new DefaultBoxAndWhiskerCategoryDataset();
 		Interpreter interpreter = SingletonInterpreter.getInterpreter();
 		ArrayList<Chunk> chunks = interpreter.getChunks();
-
+		StringBuilder buildTitle = new StringBuilder();
 		for (int j = 0; j < columns.size(); j++) {
+			buildTitle.append(" " + columns.get(j));
 			ArrayList<Double> dataList = new ArrayList<Double>();
 			for (int i = 0; i < chunks.size(); i++) {
-
-				dataList.add(getChunkData(chunks.get(i), columns.get(j)));
-
+				dataList.addAll(loopThroughChunks(chunks.get(i), columns.get(j)));
 			}
 			dataset.add(dataList, columns.get(j), " Type " + j);
 		}
-		createBoxPlot();
-
+		createBoxPlot("Boxplot with: " + buildTitle.toString());
 	}
 
-	private double getChunkData(Chunk chunk, String column) throws Exception {
+	/**
+	 * Create a dataset per chunk.
+	 * 
+	 * @param columns
+	 *            the columns which we need the data from
+	 */
+	public void setDataPerChunk(ArrayList<String> columns) {
+		dataset = new DefaultBoxAndWhiskerCategoryDataset();
+		Interpreter interpreter = SingletonInterpreter.getInterpreter();
+		ArrayList<Chunk> chunks = interpreter.getChunks();
+		StringBuilder buildTitle = new StringBuilder();
+		for (int j = 0; j < columns.size(); j++) {
+			buildTitle.append(" " + columns.get(j));
+			for (int i = 0; i < chunks.size(); i++) {
+				ArrayList<Double> dataList = new ArrayList<Double>();
+				dataList.addAll(loopThroughChunks(chunks.get(i), columns.get(j)));
+				dataset.add(dataList, columns.get(j), " Type " + i);
+			}
+		}
+		createBoxPlot("Boxplot with: " + buildTitle.toString());
+	}
+
+	/**
+	 * Get the values of the specified column of all the chunks and his
+	 * children.
+	 * 
+	 * @param currentChunk
+	 *            the chunk which we are looking at.=
+	 * @param column
+	 *            the column of the chunk we need
+	 * @return the values or values if the chunk has childs
+	 */
+	private ArrayList<Double> loopThroughChunks(Chunk currentChunk, String column) {
+		ArrayList<Double> listOfValues = new ArrayList<Double>();
+
+		if (currentChunk.hasChild()) {
+			ArrayList<Chunk> children = currentChunk.getChildren();
+			for (int i = 0; i < children.size(); i++) {
+				listOfValues.addAll(loopThroughChunks(children.get(i), column));
+			}
+		} else {
+			listOfValues.add(getChunkData(currentChunk, column));
+		}
+		return listOfValues;
+	}
+
+	private double getChunkData(Chunk chunk, String column) {
 		Db data = SingletonDb.getDb();
 		try {
 			ResultSet rs;
 			rs = data.selectResultSet("result", column,
 					"resultid = " + chunk.getLine());
-			ResultSetMetaData rsmd = rs.getMetaData();
 			String value = "";
 			if (rs.next()) {
 				value = rs.getObject(column).toString();
 			}
-
 			rs.close();
 			Double res = parseToDouble(value);
-			System.out.println(res);
 			return res;
-			// rsmd.getColumnName(i)
-			// processResultSet(rsmd.getColumnCount(), rs);
-
 		} catch (SQLException e) {
-			throw new Exception("Something went wrong creating the boxplot");
+			JOptionPane.showMessageDialog(null,
+					"Something went wrong creating the boxplot",
+					"Analyse Error", JOptionPane.WARNING_MESSAGE);
 		}
-
+		return -1;
 	}
 
+	/**
+	 * Parses the string from the db to a value.
+	 * 
+	 * @param value
+	 *            the value
+	 * @return double of the string
+	 */
 	public Double parseToDouble(String value) {
 		try {
 			return Double.parseDouble(value);
 		} catch (NumberFormatException e) {
-			System.out.println("wut");
 			return null;
 		}
 
 	}
 
-	/**
-	 * Creates a sample dataset.
-	 * 
-	 * @return A sample dataset.
-	 */
-	private BoxAndWhiskerCategoryDataset createSampleDataset() {
-
-		// number of different data series
-		final int seriesCount = 3;
-		// number of categories
-		final int categoryCount = 4;
-		// number of values added to the dataset
-		final int entityCount = 22;
-
-		DefaultBoxAndWhiskerCategoryDataset dataset = new DefaultBoxAndWhiskerCategoryDataset();
-		for (int i = 0; i < seriesCount; i++) {
-
-			for (int j = 0; j < categoryCount; j++) {
-				// list per categorie with values
-				final ArrayList<Double> list = new ArrayList<Double>();
-
-				// add some values...
-				for (int k = 0; k < entityCount; k++) {
-					final double value1 = 10.0 + Math.random() * 3;
-					list.add(new Double(value1));
-					final double value2 = 11.25 + Math.random(); // concentrate
-																	// values in
-																	// the
-																	// middle
-					list.add(new Double(value2));
-				}
-				dataset.add(list, "Series " + i, " Type " + j);
-			}
-
-		}
-		return dataset;
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == boxPlotTempRefreshButton) {
-			ArrayList<String> listOfColumns = new ArrayList<String>();
-			listOfColumns.add("value");
-			try {
-				setData(listOfColumns);
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-
-	}
+	// @Override
+	// public void actionPerformed(ActionEvent e) {
+	// if (e.getSource() == boxPlotTempRefreshButton) {
+	// ArrayList<String> listOfColumns = new ArrayList<String>();
+	// listOfColumns.add("value");
+	// // listOfColumns.add("time");
+	// try {
+	// setDataPerChunk(listOfColumns);
+	// } catch (Exception e1) {
+	// // TODO Auto-generated catch block
+	// e1.printStackTrace();
+	// }
+	// }
+	//
+	// }
 
 }
