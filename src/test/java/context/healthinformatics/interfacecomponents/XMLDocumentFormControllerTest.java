c4b93c79b137b45bd6942c04f51bd4b76284ed04
wimspaargaren <wim.spaargaren@lastmason.com>
2015-06-17 11:27:36 +0200

    Implemented the test for XMLDocumentFormController. Test coverage 99.6%.

--- src/test/java/context/healthinformatics/interfacecomponents/XMLDocumentFormControllerTest.java
+++ src/test/java/context/healthinformatics/interfacecomponents/XMLDocumentFormControllerTest.java
@@ -1,5 +1,6 @@
 package context.healthinformatics.interfacecomponents;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -20,6 +21,7 @@ import context.healthinformatics.writer.XMLDocument;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class XMLDocumentFormControllerTest {
+
 	private DocumentFieldsContainer fullDoc;
 	private DocumentFieldsContainer nullDoc;
 	private XMLDocument xmlDocFull;
@@ -28,6 +30,11 @@ public class XMLDocumentFormControllerTest {
 	private XMLDocument tempXMLDocument;
 
 	private XMLEditor edit = new XMLEditor(mock(InputPage.class));
+	private DocumentFieldsContainer mockedDocField;
+	private ArrayList<DocumentFieldsContainer> mockedList;
+
+	private ColumnFieldContainer mockedColField;
+	private ArrayList<ColumnFieldContainer> mockedListColField;
 
 	/**
 	 * Set up some documents to work with.
@@ -45,12 +52,45 @@ public class XMLDocumentFormControllerTest {
 		fullDoc = new DocumentFieldsContainer(xmlDocFull, edit);
 		nullDoc = new DocumentFieldsContainer(xmlDocNull, edit);
 		listOfDocuments = new ArrayList<DocumentFieldsContainer>();
+		setupMockedDocumentContainer();
+		setupMockedColumnContainer();
+	}
 
+	/**
+	 * Setup the mocked document container to test the different exceptions.
+	 */
+	public void setupMockedDocumentContainer() {
+		mockedList = new ArrayList<DocumentFieldsContainer>();
+		mockedDocField = mock(DocumentFieldsContainer.class);
+		when(mockedDocField.getDocumentNameValue()).thenReturn("name");
+		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
+		when(mockedDocField.getDocumentTypeValue()).thenReturn("excel");
+		when(mockedDocField.getDocumentStartLineValue()).thenReturn("1");
+		when(mockedDocField.getDelimiterValue()).thenReturn("");
+		when(mockedDocField.getSheetValue()).thenReturn("1");
+		when(mockedDocField.getColumnFields()).thenReturn(
+				new ArrayList<ColumnFieldContainer>());
+		mockedList.add(mockedDocField);
+	}
+
+	/**
+	 * Setup the mocked column container to test the different exceptions.
+	 */
+	public void setupMockedColumnContainer() {
+		mockedColField = mock(ColumnFieldContainer.class);
+		mockedListColField = new ArrayList<ColumnFieldContainer>();
+		mockedListColField.add(mockedColField);
+		when(mockedColField.getColumnIDValue()).thenReturn("1");
+		when(mockedColField.getColumnNameValue()).thenReturn("colName");
+		when(mockedColField.getColumnTypeValue()).thenReturn("int");
+		when(mockedColField.getColumnDateTypeValue()).thenReturn("yymmdd");
 	}
 
 	/**
 	 * Test a correct document.
-	 * @throws Exception shouldnt throw an exception
+	 * 
+	 * @throws Exception
+	 *             shouldn't throw an exception
 	 */
 	@Test
 	public void testCorrectListOfDocuments() throws Exception {
@@ -62,7 +102,9 @@ public class XMLDocumentFormControllerTest {
 
 	/**
 	 * Test a correct list of documents which is empty.
-	 * @throws Exception shouldnt throw an exception
+	 * 
+	 * @throws Exception
+	 *             shouldn't throw an exception
 	 */
 	@Test
 	public void testCorrectEmptyListOfDocuments() throws Exception {
@@ -73,7 +115,9 @@ public class XMLDocumentFormControllerTest {
 
 	/**
 	 * Test a null document.
-	 * @throws Exception shouldnt throw an exception
+	 * 
+	 * @throws Exception
+	 *             shouldnt throw an exception
 	 */
 	@Test
 	public void testCorrectListOfNullDocuments() throws Exception {
@@ -85,7 +129,9 @@ public class XMLDocumentFormControllerTest {
 
 	/**
 	 * Test doc type is empty.
-	 * @throws Exception should throw exception with doc type is empty
+	 * 
+	 * @throws Exception
+	 *             should throw exception with doc type is empty
 	 */
 	@Test
 	public void testEmptyDocType() throws Exception {
@@ -101,192 +147,216 @@ public class XMLDocumentFormControllerTest {
 
 	/**
 	 * Test doc name is empty.
-	 * @throws Exception should throw exception with doc name is empty
+	 * 
+	 * @throws Exception
+	 *             should throw exception with doc name is empty
 	 */
 	@Test(expected = Exception.class)
 	public void testEmptyDocName() throws Exception {
-		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
 		when(mockedDocField.getDocumentNameValue()).thenReturn("");
-		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
-		when(mockedDocField.getDocumentTypeValue()).thenReturn("excel");
-		when(mockedDocField.getDocumentStartLineValue()).thenReturn("1");
-		when(mockedDocField.getDelimiterValue()).thenReturn("");
-		when(mockedDocField.getSheetValue()).thenReturn("1");
-		when(mockedDocField.getColumnFields()).thenReturn(
-				new ArrayList<ColumnFieldContainer>());
-		mockedList.add(mockedDocField);
 		XMLDocumentFormController controller = new XMLDocumentFormController(
 				mockedList);
-		controller.checkIfHasEmptyFields();
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals("Error in document:1 Document name is empty!",
+					e.getMessage());
+			throw new Exception("Test Passes!");
+		}
 	}
 
 	/**
 	 * Test Doc path is empty.
-	 * @throws Exception should throw exception with doc path is empty
+	 * 
+	 * @throws Exception
+	 *             should throw exception with doc path is empty
 	 */
 	@Test(expected = Exception.class)
 	public void testEmptyDocPath() throws Exception {
-		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
-		when(mockedDocField.getDocumentNameValue()).thenReturn("docName");
 		when(mockedDocField.getDocumentPathValue()).thenReturn("");
-		when(mockedDocField.getDocumentTypeValue()).thenReturn("excel");
-		when(mockedDocField.getDocumentStartLineValue()).thenReturn("1");
-		when(mockedDocField.getDelimiterValue()).thenReturn("");
-		when(mockedDocField.getSheetValue()).thenReturn("1");
-		when(mockedDocField.getColumnFields()).thenReturn(
-				new ArrayList<ColumnFieldContainer>());
-		mockedList.add(mockedDocField);
 		XMLDocumentFormController controller = new XMLDocumentFormController(
 				mockedList);
-		controller.checkIfHasEmptyFields();
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals("Error in document:1 Document path is empty!",
+					e.getMessage());
+			throw new Exception("Test Passes!");
+		}
 	}
 
 	/**
 	 * Test doc start line is empty.
-	 * @throws Exception should throw exception wih doc path is empty
+	 * 
+	 * @throws Exception
+	 *             should throw exception wih doc path is empty
 	 */
 	@Test(expected = Exception.class)
 	public void testEmptyDocStartLine() throws Exception {
-		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
-		when(mockedDocField.getDocumentNameValue()).thenReturn("docName");
-		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
-		when(mockedDocField.getDocumentTypeValue()).thenReturn("excel");
 		when(mockedDocField.getDocumentStartLineValue()).thenReturn("");
-		when(mockedDocField.getDelimiterValue()).thenReturn(",");
-		when(mockedDocField.getSheetValue()).thenReturn("1");
-		when(mockedDocField.getColumnFields()).thenReturn(
-				new ArrayList<ColumnFieldContainer>());
-		mockedList.add(mockedDocField);
 		XMLDocumentFormController controller = new XMLDocumentFormController(
 				mockedList);
-		controller.checkIfHasEmptyFields();
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Document start line is empty or not a number!",
+					e.getMessage());
+			throw new Exception("Test Passes!");
+		}
 	}
 
 	/**
 	 * Test Empty Doc sheet.
-	 * @throws Exception should throw exception with empty doc sheet
+	 * 
+	 * @throws Exception
+	 *             should throw exception with empty doc sheet
 	 */
 	@Test(expected = Exception.class)
 	public void testEmptyDocSheet() throws Exception {
-		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
-		when(mockedDocField.getDocumentNameValue()).thenReturn("docName");
-		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
 		when(mockedDocField.getDocumentTypeValue()).thenReturn("excel");
-		when(mockedDocField.getDocumentStartLineValue()).thenReturn("1");
-		when(mockedDocField.getDelimiterValue()).thenReturn("");
 		when(mockedDocField.getSheetValue()).thenReturn("");
-		when(mockedDocField.getColumnFields()).thenReturn(
-				new ArrayList<ColumnFieldContainer>());
-		mockedList.add(mockedDocField);
 		XMLDocumentFormController controller = new XMLDocumentFormController(
 				mockedList);
-		controller.checkIfHasEmptyFields();
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Document sheet value is empty or not a number!",
+					e.getMessage());
+			throw new Exception("Test Passes!");
+		}
 	}
 
-	/**|
-	 * Test empty delimiter.
-	 * @throws Exception should throw exception with empty delimiter
+	/**
+	 * | Test empty delimiter.
+	 * 
+	 * @throws Exception
+	 *             should throw exception with empty delimiter
 	 */
 	@Test(expected = Exception.class)
 	public void testEmptyDelimiter() throws Exception {
-		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
-		when(mockedDocField.getDocumentNameValue()).thenReturn("docName");
-		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
 		when(mockedDocField.getDocumentTypeValue()).thenReturn("txt");
-		when(mockedDocField.getDocumentStartLineValue()).thenReturn("1");
 		when(mockedDocField.getDelimiterValue()).thenReturn("");
-		when(mockedDocField.getSheetValue()).thenReturn("");
-		when(mockedDocField.getColumnFields()).thenReturn(
-				new ArrayList<ColumnFieldContainer>());
-		mockedList.add(mockedDocField);
 		XMLDocumentFormController controller = new XMLDocumentFormController(
 				mockedList);
-		controller.checkIfHasEmptyFields();
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Document delimiter value is empty!",
+					e.getMessage());
+			throw new Exception("Test Passes!");
+		}
 	}
 
 	/**
 	 * Test with not number for Start line.
-	 * @throws Exception throws exception for wrong start line
+	 * 
+	 * @throws Exception
+	 *             throws exception for wrong start line
 	 */
 	@Test(expected = Exception.class)
 	public void testWrongFormattedStartLine() throws Exception {
-		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
-		when(mockedDocField.getDocumentNameValue()).thenReturn("docName");
-		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
-		when(mockedDocField.getDocumentTypeValue()).thenReturn("txt");
-		when(mockedDocField.getDocumentStartLineValue()).thenReturn("wrongFormattedLine");
-		when(mockedDocField.getDelimiterValue()).thenReturn(",");
-		when(mockedDocField.getSheetValue()).thenReturn("1");
-		when(mockedDocField.getColumnFields()).thenReturn(
-				new ArrayList<ColumnFieldContainer>());
-		mockedList.add(mockedDocField);
+		when(mockedDocField.getDocumentStartLineValue()).thenReturn(
+				"wrongFormattedLine");
 		XMLDocumentFormController controller = new XMLDocumentFormController(
 				mockedList);
-		controller.checkIfHasEmptyFields();
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Document start line is empty or not a number!",
+					e.getMessage());
+			throw new Exception("Test Passes!");
+		}
 	}
 
 	/**
 	 * Test sheet with wrong formatted number.
-	 * @throws Exception should throw error 
+	 * 
+	 * @throws Exception
+	 *             should throw error
 	 */
 	@Test(expected = Exception.class)
 	public void testWrongFormattedSheet() throws Exception {
-		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
-		when(mockedDocField.getDocumentNameValue()).thenReturn("docName");
-		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
-		when(mockedDocField.getDocumentTypeValue()).thenReturn("excel");
-		when(mockedDocField.getDocumentStartLineValue()).thenReturn("1");
-		when(mockedDocField.getDelimiterValue()).thenReturn("");
 		when(mockedDocField.getSheetValue()).thenReturn("wrongFormattedLine");
-		when(mockedDocField.getColumnFields()).thenReturn(
-				new ArrayList<ColumnFieldContainer>());
-		mockedList.add(mockedDocField);
 		XMLDocumentFormController controller = new XMLDocumentFormController(
 				mockedList);
-		controller.checkIfHasEmptyFields();
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Document sheet value is empty or not a number!",
+					e.getMessage());
+			throw new Exception("Test Passes!");
+		}
 	}
 
-//	@Test(expected = Exception.class)
-//	public void testEmptyColumnID() throws Exception {
-//		
-//		ColumnFieldContainer mockedColField = mock(ColumnFieldContainer.class);
-//		ArrayList<ColumnFieldContainer> mockedListColField = new ArrayList<ColumnFieldContainer>();
-//		mockedListColField.add(mockedColField);
-//		when(mockedColField.getColumnIDValue()).thenReturn("");
-//		when(mockedColField.getColumnNameValue()).thenReturn("colName");
-//		when(mockedColField.getColumnTypeValue()).thenReturn("int");
-//		when(mockedColField.getColumnDateTypeValue()).thenReturn("");
-//		
-//		ArrayList<DocumentFieldsContainer> mockedList = new ArrayList<DocumentFieldsContainer>();
-//		DocumentFieldsContainer mockedDocField = mock(DocumentFieldsContainer.class);
-//		when(mockedDocField.getDocumentNameValue()).thenReturn("docName");
-//		when(mockedDocField.getDocumentPathValue()).thenReturn("path");
-//		when(mockedDocField.getDocumentTypeValue()).thenReturn("excel");
-//		when(mockedDocField.getDocumentStartLineValue()).thenReturn("1");
-//		when(mockedDocField.getDelimiterValue()).thenReturn("");
-//		when(mockedDocField.getSheetValue()).thenReturn("wrongFormattedLine");
-//		when(mockedDocField.getColumnFields()).thenReturn(
-//				mockedListColField);
-//		
-//		XMLDocumentFormController controller = new XMLDocumentFormController(
-//				mockedList);
-//		controller.checkIfHasEmptyFields();
-//	}
+	/**
+	 * Test empty column id.
+	 * 
+	 * @throws Exception
+	 *             should throw exception for empty column id
+	 */
+	@Test(expected = Exception.class)
+	public void testEmptyColumnID() throws Exception {
+		when(mockedDocField.getColumnFields()).thenReturn(mockedListColField);
+		when(mockedColField.getColumnIDValue()).thenReturn("");
+		XMLDocumentFormController controller = new XMLDocumentFormController(
+				mockedList);
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Error in column: 1 Column id is empty or not a number!",
+					e.getMessage());
+			throw new Exception("Test passes!");
+		}
+	}
 
-//	@Test(expected = Exception.class)
-//	public void testEmptyColumnName() {
-//
-//	}
-//
-//	@Test(expected = Exception.class)
-//	public void testEmptyColumnDateType() {
-//
-//	}
+	/**
+	 * Test a column with empty name.
+	 * 
+	 * @throws Exception
+	 *             should throw exception for empty column name
+	 */
+	@Test(expected = Exception.class)
+	public void testEmptyColumnName() throws Exception {
+		when(mockedDocField.getColumnFields()).thenReturn(mockedListColField);
+		when(mockedColField.getColumnNameValue()).thenReturn("");
+		XMLDocumentFormController controller = new XMLDocumentFormController(
+				mockedList);
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Error in column: 1 Column name is empty!",
+					e.getMessage());
+			throw new Exception("Test passes!");
+		}
+	}
+
+	/**
+	 * Test a column with empty column date type.
+	 * 
+	 * @throws Exception
+	 *             should throw exception for empty column date type
+	 */
+	@Test(expected = Exception.class)
+	public void testEmptyColumnDateType() throws Exception {
+		when(mockedDocField.getColumnFields()).thenReturn(mockedListColField);
+		when(mockedColField.hasDateType()).thenReturn(true);
+		when(mockedColField.getColumnDateTypeValue()).thenReturn("");
+		XMLDocumentFormController controller = new XMLDocumentFormController(
+				mockedList);
+		try {
+			controller.checkIfHasEmptyFields();
+		} catch (Exception e) {
+			assertEquals(
+					"Error in document:1 Error in column: 1 Column date type is empty!",
+					e.getMessage());
+			throw new Exception("Test passes!");
+		}
+	}
 }
